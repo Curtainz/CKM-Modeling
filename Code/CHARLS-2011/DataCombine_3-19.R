98e328e9fb5c04b115f3e328b7ac742a23a02ac8
Zhang Cang <zielene@OUTLOOK.COM>
2025-03-23 20:01:20 +0800

    Reconstruct code & Processed data

--- Code/CHARLS-2011/DataCombine_3-19.R
+++ Code/CHARLS-2011/DataCombine_3-19.R
@@ -11,13 +11,13 @@ biodata <- read_dta("D:/GitHub/CKM-Modeling/Datasets/CHARLS-2011/RawData/Active/
 extracted_biodata <- biodata %>% select(ID, householdID, communityID, qi002, pl001, ql002, qm002, qa003, qa007, qa011, qa004, qa008, qa012)
 
 # 统计行数
-# num_rows_biodata <- nrow(extracted_biodata)
+#num_rows_biodata <- nrow(extracted_biodata)
 
 # 查找统计缺失值
-# num_height_mis <- sum(is.na(extracted_biodata$qi002))
-# num_WeightJudge_mis <- sum(is.na(extracted_biodata$pl001))
-# num_weight_mis <- sum(is.na(extracted_biodata$ql002))
-# num_waist_mis <- sum(is.na(extracted_biodata$qm002))
+#num_height_mis <- sum(is.na(extracted_biodata$qi002))
+#num_WeightJudge_mis <- sum(is.na(extracted_biodata$pl001))
+#num_weight_mis <- sum(is.na(extracted_biodata$ql002))
+#num_waist_mis <- sum(is.na(extracted_biodata$qm002))
 
 #清理变量
 rm(biodata)
@@ -27,18 +27,18 @@ rm(biodata)
 # 读取健康状况数据
 healthdata <- read_dta("D:/GitHub/CKM-Modeling/Datasets/CHARLS-2011/RawData/Active/health_status_and_functioning.dta")
 
-# 从healthdata中提取<个人ID, 家庭ID, 社区ID, 高血压(1=yes, 2=no, 后同), 高血压再确认, 糖尿病, 心脏病, 中风, 肾病>
-extracted_healthdata <- healthdata %>% select(ID, householdID, communityID, da007_1_, da008_1_, da007_3_, da007_7_, da007_8_, da007_9_)
+# 从healthdata中提取<个人ID, 家庭ID, 社区ID, 高血压(1=yes, 2=no, 后同), 高血压再确认, 糖尿病, 心脏病, 中风, 肾病, 降压药情况(中), 降压药情况(西)
+extracted_healthdata <- healthdata %>% select(ID, householdID, communityID, da007_1_, da008_1_, da007_3_, da007_7_, da007_8_, da007_9_, da011s1, da011s2)
 
 # 统计行数
-# num_rows_healthdata <- nrow(extracted_healthdata)
+#num_rows_healthdata <- nrow(extracted_healthdata)
 
 # 查找统计缺失值
-# num_hyperten_mis <- sum(is.na(extracted_healthdata$da007_1_))
-# num_hypertenconfirm_mis <- sum(is.na(extracted_healthdata$da008_1_))
-# num_sugar_mis <- sum(is.na(extracted_healthdata$da007_3_))
-# num_heartatt_mis <- sum(is.na(extracted_healthdata$da007_7_))
-# num_kidneydis_mis <- sum(is.na(extracted_healthdata$da007_9_))
+#num_hyperten_mis <- sum(is.na(extracted_healthdata$da007_1_))
+#num_hypertenconfirm_mis <- sum(is.na(extracted_healthdata$da008_1_))
+#num_sugar_mis <- sum(is.na(extracted_healthdata$da007_3_))
+#num_heartatt_mis <- sum(is.na(extracted_healthdata$da007_7_))
+#num_kidneydis_mis <- sum(is.na(extracted_healthdata$da007_9_))
 
 #清理变量
 rm(healthdata)
@@ -52,13 +52,13 @@ blooddata <- read_dta("D:/GitHub/CKM-Modeling/Datasets/CHARLS-2011/RawData/Activ
 extracted_blooddata <- blooddata %>% select(ID, newhba1c, newhdl, newtg, newglu)
 
 # 统计行数
-# num_rows_blooddata <- nrow(extracted_blooddata)
+#num_rows_blooddata <- nrow(extracted_blooddata)
 
 # 查找统计缺失值
-# num_hba1c_mis <- sum(is.na(extracted_blooddata$newhba1c))
-# num_hdl_mis <- sum(is.na(extracted_blooddata$newhdl))
-# num_tg_mis <- sum(is.na(extracted_blooddata$newtg))
-# num_glu_mis <- sum(is.na(extracted_blooddata$newglu))
+#num_hba1c_mis <- sum(is.na(extracted_blooddata$newhba1c))
+#num_hdl_mis <- sum(is.na(extracted_blooddata$newhdl))
+#num_tg_mis <- sum(is.na(extracted_blooddata$newtg))
+#num_glu_mis <- sum(is.na(extracted_blooddata$newglu))
 
 #清理变量
 rm(blooddata)
@@ -74,8 +74,5 @@ merged_data_1 <- full_join(extracted_biodata, extracted_healthdata, by = merge_c
 # 合并血检数据
 merged_data_2 <- full_join(merged_data_1, extracted_blooddata, by = "ID")
 
-# 剔除<身高, 腰围, 血检四项数据>缺失值
-cleaned_data_1 <- merged_data_2 %>% filter(!is.na(qi002) & !is.na(qm002) & !is.na(newglu) & !is.na(newhba1c) & !is.na(newhdl) & !is.na(newtg))
-
 # 导出数据
-write_dta(cleaned_data_1, "D:/GitHub/CKM-Modeling/ProcessedData/CHARLS-2011/2025.3.19/combined_data.dta")
+write_dta(merged_data_2, "D:/GitHub/CKM-Modeling/ProcessedData/CHARLS-2011/2025.3.19/combined_data.dta")
--- Code/CHARLS-2015/Cleaning2.R
+++ Code/CHARLS-2015/Cleaning2.R
@@ -20,8 +20,26 @@ data <- data %>%
     diastolic = qa004
   )
 
+#################
+##身高/体重/BMI##
+#################
+
 # 将身高小于10的每个数据乘以100，并覆盖原数据
-data <- data %>% mutate(height = ifelse(height < 10, height * 100, height))
+#data <- data %>% mutate(height = ifelse(height < 10, height * 100, height))
+
+# 身高异常值标记，规则：小于80或大于200
+data <- data %>%
+  mutate(
+    height_def = case_when(
+      is.na(height) ~ "outlier",
+      height < 80 ~ "outlier",
+      height > 200 ~ "outlier",
+      TRUE ~ "normal"
+    )
+  )
+
+# 移动列
+data <- data %>% select(ID, householdID, communityID, height, height_def, everything())
 
 # 更新体重值，如果体重判断为5，则赋值为150
 data <- data %>%
@@ -34,13 +52,52 @@ data <- data %>%
 data <- data %>% select(-pl001)
 
 # 剔除体重空值行
-data <- data %>% filter(!is.na(weight))
+#data <- data %>% filter(!is.na(weight))
 
 # 剔除身高体重异常值
-data <- data %>% filter(height >= 80 & height <= 200 & weight >= 10)
+#data <- data %>% filter(height >= 80 & height <= 200 & weight >= 10)
+
+# 体重异常值标记，规则：小于20或大于150
+data <- data %>%
+  mutate(
+    weight_def = case_when(
+      is.na(weight) ~ "outlier",
+      weight < 20 ~ "outlier",
+      weight > 150 ~ "outlier",
+      TRUE ~ "normal"
+    )
+  )
+
+# 移动列
+data <- data %>% select(ID, householdID, communityID, height, height_def, weight, weight_def, everything())
 
 # 计算 BMI 值，BMI = 体重(kg) / (身高(m))^2
 data$BMI <- data$weight / (data$height / 100)^2
+
+# 移动列
+data <- data %>% select(ID, householdID, communityID, height, height_def, weight, weight_def, BMI, everything())
+
+######
+#腰围#
+######
+
+# 腰围异常值标记，规则：小于40或大于200
+data <- data %>%
+  mutate(
+    waist_def = case_when(
+      is.na(waist) ~ "outlier",
+      waist < 40 ~ "outlier",
+      waist > 200 ~ "outlier",
+      TRUE ~ "normal"
+    )
+  )
+
+# 移动列
+data <- data %>% select(ID, householdID, communityID, height, height_def, weight, weight_def, BMI, waist, waist_def, everything())
+
+######
+#血压#
+######
 
 # 计算每个人的低压平均值，不包括异常值和缺失值
 data <- data %>%
@@ -61,6 +118,24 @@ data <- data %>%
 # 剔除血压
 data <- data %>% select(-qa007, -qa008, -qa011, -qa012)
 
+# 血压异常值标记，规则：收缩压小于舒张压、收缩压小于50或大于300、舒张压小于30或大于180
+data <- data %>%
+  mutate(
+    bp_def = case_when(
+      is.na(systolic) ~ "outlier",
+      is.na(diastolic) ~ "outlier",
+      systolic < 50 ~ "outlier",
+      systolic > 300 ~ "outlier",
+      diastolic < 30 ~ "outlier",
+      diastolic > 180 ~ "outlier",
+      systolic < diastolic ~ "outlier",
+      TRUE ~ "normal"
+    )
+  )
+
+# 移动列
+data <- data %>% select(ID, householdID, communityID, height, height_def, weight, weight_def, BMI, waist, waist_def, systolic, diastolic, bp_def, everything())
+
 # 更新血压，如果血压确认为1，则赋值为1
 data <- data %>%
   mutate(hypert = case_when(
@@ -72,14 +147,26 @@ data <- data %>%
 data <- data %>% select(-da008_1_)
 
 # 缺失值填充
+#data <- data %>%
+#  mutate(
+#    hypert = ifelse(is.na(hypert), 2, hypert),
+#    diabetes_hbs = ifelse(is.na(diabetes_hbs), 2, diabetes_hbs),
+#    heart_disease = ifelse(is.na(heart_disease), 2, heart_disease),
+#    stroke = ifelse(is.na(stroke), 2, stroke),
+#    kidney_disease = ifelse(is.na(kidney_disease), 2, kidney_disease)
+#  )
+
+# 检查用药情况
 data <- data %>%
   mutate(
-    hypert = ifelse(is.na(hypert), 2, hypert),
-    diabetes_hbs = ifelse(is.na(diabetes_hbs), 2, diabetes_hbs),
-    heart_disease = ifelse(is.na(heart_disease), 2, heart_disease),
-    stroke = ifelse(is.na(stroke), 2, stroke),
-    kidney_disease = ifelse(is.na(kidney_disease), 2, kidney_disease)
+    hyp_med = ifelse(da011s1 == 1 | da011s2 == 1, 1, NA)
   )
+
+# 剔除中西药列
+data <- data %>% select(-da011s1, -da011s2)
+
+# 移动列
+data <- data %>% select(ID, householdID, communityID, height, height_def, weight, weight_def, BMI, waist, waist_def, systolic, diastolic, bp_def, hypert, hyp_med, diabetes_hbs, heart_disease, stroke, kidney_disease, everything())
 
 # 将ID列转换为字符类型，以便提取最后一位
 data$ID <- as.character(data$ID)
